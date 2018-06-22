44e83db2b0a1adaaa195abb36a512201e7ca23b3
Sergio <srgmatveev@gmail.com>
2018-06-22 16:43:52 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	cout << "Hello, World!" << endl;
+	cout << "Version = " << version() << endl;
+	return 0;
+}
